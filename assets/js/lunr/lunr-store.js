aa8b0b8d85e2e4cd88fb1c535fc0487cea472800
edk <edk@users.noreply.github.com>
2022-04-05 16:16:22 +0000

    published by GitHub Actions

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "First post! (Kinda)",
+        "excerpt":"¡Holá! Hello unlikely visitor! Welcome to the long-overdue reboot of my blog. The first version of this was an experiment in 2012, with this new-fangled tool called Jekyll. After a couple of cringe-worthy posts (not archived by the wayback machine, thankfully) I didn’t touch this again for a decade. So...","categories": [],
+        "tags": ["site","jekyll","github","docker"],
+        "url": "/first-post/",
+        "teaser": null
+      },{
+        "title": "Github actions and CI",
+        "excerpt":"I always seem to find myself yak-shaving, and restarting this blog is no different. If I just wanted to get a blog up, with minimal fuss, I could have used a free Wordpress site, or setup any number of free or low cost self-hosted options. But for the challenge, and...","categories": [],
+        "tags": ["github","CI"],
+        "url": "/github-actions-and-ci/",
+        "teaser": null
+      },{
+        "title": "10g Home Networking",
+        "excerpt":"Goal Upgrade from 1G to 10GbE in my tiny home netowrk. Current Setup Netgate 2100 running pfSense and a UniFi UAP AC-PRO WiFi access point SuperMicro based NAS/media center with i3 cpu, ECC RAM, ZFS filesystem, running containers, including Pi-hole. random netgear and d-link unmanaged switches Why? I switched to...","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/10g-home-networking/",
+        "teaser": null
+      },{
+        "title": "Benchmarking Io",
+        "excerpt":"The main reason to upgrade my networking speed is to see improved throughput and latencies (duh). But I want to know, how much does it help in real-world usage? As I wait to pull the trigger on ordering the equipment, my inner-yak-shaver comes out again. I’ve used CrystalDiskMark in the...","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/benchmarking-io/",
+        "teaser": null
+      },{
+        "title": "Omg So Slow",
+        "excerpt":"Just a short rant, and incredulity. I must be doing something wrong, because why would anyone willingly put up with slow build times? Let me back up a bit. I was messing around with tailwindcss in a test jekyll project, and I was getting used to and liked saving the...","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/omg-so-slow/",
+        "teaser": null
+      },{
+        "title": "The Joy Of Ruby And Rails",
+        "excerpt":" ","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/the-joy-of-ruby-and-rails/",
+        "teaser": null
+      },{
+        "title": "Followup Tailwind And Status",
+        "excerpt":"Not that anyone cares, I’m doing this as a kind of public journal and a way to keep myself honest, and continue to post, even when not much of note has happened. tailwindcss For some reason, I wasn’t able to find a super quick feedback cycle that I was happy...","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/followup-tailwind-and-status/",
+        "teaser": null
+      },{
+        "title": "The Joy Of Ruby And Rails",
+        "excerpt":"One thing that my curmudgeonly-self has had issues with lately (the past few years) is the pain I feel when dealing with the JS ecosystem. Let me give some background to explain. I’ve been using Ruby and Rails for over 10 years now. I originally found it when searching for...","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/the-joy-of-ruby-and-rails/",
+        "teaser": null
+      }]
